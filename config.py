65793e553a90e51a7a40a6492e42835904f2928e
esh-bc <singhyashraj432.singhyashraj.3@gmail.com>
2026-09-21 17:53:41 +0530

    Update config.py

--- config.py
+++ config.py
@@ -6,8 +6,8 @@
 BOT_TOKEN = "8948036520:AAEw82L7J6E38MPvYZSkajqdWpuFL4CP37I"
 
 # Get from https://my.telegram.org
-API_ID = 123456
-API_HASH = "YOUR_API_HASH_HERE"
+API_ID = 30439917
+API_HASH = "4f408081dbb976a9943ada5b551288b7"
 
 # Get from https://gplinks.in/member/tools/api
 GPLINKS_API = "YOUR_GPLINKS_API_KEY"
